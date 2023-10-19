b28579044860abbacc89ea7858547bbdde8c7198
EmoKiller <Emokiller22101993@gmail.com>
2023-10-19 10:33:36 +0700

    no message

--- Assets/Script/Manager/UIToggle.cs
+++ Assets/Script/Manager/UIToggle.cs
@@ -1,61 +1,18 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIToggle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class UIToggle : MonoBehaviour, IPointerEnterHandler
 {
-    public MenuType type;
-    public Action<MenuType> OnSelected = null;
-    [SerializeField] Sprite _sprite;
-    [SerializeField] Image image;
-    [SerializeField] TMP_Text text;
-    private Toggle toggle = null;
-
-
-    private void Awake()
+    public TMP_Text text;
+    public Image uiImg;
+    public virtual void OnPointerEnter(PointerEventData eventData)
     {
-        image = GetComponent<Image>();
-        toggle = GetComponent<Toggle>();
-        toggle.onValueChanged.AddListener(OnValueChanged);
+        ToggleManager.ToggleAction?.Invoke(this);
     }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        text.color = Color.white;
-        ToggleOn();
-        
-    }
-    public void OnPointerExit(PointerEventData eventData)
-    {
-
-    }
-    private void OnValueChanged(bool isOn)
-    {
-        image = GetComponent<Image>();
-        
-        image.sprite = _sprite;
-        if (isOn)
-        {
-            OnSelected?.Invoke(type);
-        }
-            
-        
-
-        //if (iconFocus != null)
-        //    iconFocus.SetActive(isOn);
-
-        //if (tabFocus != null)
-        //    tabFocus.SetActive(isOn);
-
-        //if (isOn)
-        //    OnSelected?.Invoke(type);
-    }
-    public void ToggleOn() => toggle.isOn = true;
-    public void ToggleOff() => toggle.isOn = false;
 
     
 }
